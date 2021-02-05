39e45da358f716caafcc56daeab16677f2a16106
Joseba Alonso <7daysofrain@gmail.com>
2021-02-05 18:23:03 +0100

    first setup

--- index.js
+++ index.js
@@ -10,14 +10,40 @@ const effectMap = config.get('effectMap');
 
 for (const [key, value] of Object.entries(effectMap)) {
     console.log(`${key}: ${value}`);
-    f1.on(`${key}:pressed`,() => hyperion.setEffect(value.name));
+    f1.on(`${key}:pressed`,() => hyperion.setEffect(value.name, value.args));
     f1.setRGB(key,...value.ledColor);
 }
 
 const hyperion = new Hyperion( config.get('hyperion'), 19444 );
 hyperion.on('connect', function(){
     console.log('connected');
+    hyperion.sendMessage('serverinfo', e => console.log(e));
 });
 hyperion.on('error', function(err){
     console.error('oops...', err);
 });
+
+
+let strobeInterval;
+let strobeState = false;
+f1.on('p3:pressed',startStrobe);
+f1.on('p3:released',stopStrobe);
+f1.setRGB('p3', ...[255,0,0])
+function startStrobe(){
+    strobeInterval = setInterval(strobe,300);
+}
+function stopStrobe(){
+    clearInterval(strobeInterval);
+}
+function strobe(){
+    strobeState = !strobeState;
+    if(strobeState){
+        hyperion.setColor([255,0,0]);
+        f1.setRGB('p3', ...[255,0,0]);
+    }
+    else{
+        hyperion.setColor([0,0,0]);
+        f1.setRGB('p3', ...[0,0,0]);
+    }
+}
+f1.on('sync:pressed',() => hyperion.clearall())
