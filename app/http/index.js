ea5aeecfcd04904be8c83d67225dbefd74d697ba
Joseba Alonso <7daysofrain@gmail.com>
2021-03-16 21:52:03 +0100

    starting Web UI

---
+++ app/http/index.js
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+
+import config from 'config';
+import http from 'http';
+import express from 'express';
+
+const debug = console.log;
+
+export default function createServer(){
+    const app = express();
+    const server = http.createServer(app);
+    server.listen(config.get('http-port'));
+    server.on('error', onError);
+    server.on('listening', onListening);
+
+
+    /**
+     * Event listener for HTTP server "error" event.
+     */
+
+    function onError(error) {
+        if (error.syscall !== 'listen') {
+            throw error;
+        }
+
+        const bind = typeof port === 'string'
+            ? 'Pipe ' + port
+            : 'Port ' + port;
+
+        // handle specific listen errors with friendly messages
+        switch (error.code) {
+            case 'EACCES':
+                console.error(bind + ' requires elevated privileges');
+                throw error;
+                break;
+            case 'EADDRINUSE':
+                console.error(bind + ' is already in use');
+                throw error;
+                break;
+            default:
+                throw error;
+        }
+    }
+    /**
+     * Event listener for HTTP server "listening" event.
+     */
+
+    function onListening() {
+        const addr = server.address();
+        const bind = typeof addr === 'string'
+            ? 'pipe ' + addr
+            : 'port ' + addr.port;
+        debug('Listening on ' + bind);
+    }
+    
+    return server;
+};
--- app/index.js
+++ app/index.js
@@ -1,7 +1,9 @@
 import HyperionView from "./views/HyperionView.js";
 import f1Navigator from "./views/f1/f1Navigator.js";
 import DancingPIView from "./views/DancingPIView.js";
+import createServer from "./http/index.js";
 
 f1Navigator.navigate('home');
+const httpServer = createServer();;
 const hyperionView = new HyperionView();
 const dancingPiView = new DancingPIView();
