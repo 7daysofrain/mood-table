f45f13f458a582097b0669622fb5ae1bc8e6f614
Joseba Alonso <hola@josebaalonso.tech>
2022-10-28 06:03:46 +0200

    firmware start

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# Mood Table Project
+
 #Image setup
 
 Despues de meter la imagen de Raspberry OS. Para habilitar la red:
@@ -16,3 +18,14 @@ network={
 
 Enable SSH on a headless Raspberry Pi (add file to SD card on another machine)
    For headless setup, SSH can be enabled by placing a file named ssh, without any extension, onto the boot partition of the SD card from another computer. When the Pi boots, it looks for the ssh file. If it is found, SSH is enabled and the file is deleted. The content of the file does not matter; it could contain text, or nothing at all.
+
+## Screen
+
+Importante antes
+```
+sudo apt update
+```
+ Las instrucciones
+ https://tengfone.medium.com/setting-up-raspberry-pi-4-3-5-touch-screen-xpt2046-349e484a7813
+ 
+## Kiosk mode
--- packages/controller/src/index.js
+++ packages/controller/src/index.js
@@ -4,6 +4,7 @@ import DancingPIView from "./views/DancingPIView.js";
 import createServer from "./http/index.js";
 import wss from "@mood-table/shared/src/wss-bridge/server.js";
 import appState from "@mood-table/shared/src/models/app-state.js";
+import {PythonShell} from "python-shell";
 
 
 //f1Navigator.navigate('home');
@@ -15,3 +16,30 @@ const dancingPiView = new DancingPIView();
 sockets.addEventListener('changeFX', ev => {
     appState.setFX(ev.data.message);
 })
+
+/**
+ * Run the python main shell
+ */
+let pyShell
+const pythonDefaults = {
+    mode: 'text',
+    pythonOptions: ['-u'],
+    scriptPath: process.cwd(),
+};
+function runPython(file, args) {
+    const options = args ? {...pythonDefaults, args} : pythonDefaults;
+    if(pyShell) {
+        pyShell.kill();
+    }
+    pyShell = PythonShell.run(file, options, function (err, results) {
+        //On 'results' we get list of strings of all print done in your py scripts sequentially.
+        if (err) throw err;
+        if(results){
+            console.log('results: ');
+            for (let i of results) {
+                console.log(i, "---->", typeof i)
+            }
+        }
+    });
+}
+runPython('python/main.py');
